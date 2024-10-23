e09674fd8f6f6c94562fce2313569d37eb09e360
jmokrzycki <jarek.mokrzycki@gmail.com>
2024-10-23 20:55:58 +0200

    06

--- 03/server.js
+++ 03/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // console.log(req.url, req.method);
 
   // res.setHeader("Content-Type", "text/plain");
   // res.write("hello, ninjas");
@@ -15,8 +15,30 @@ const server = http.createServer((req, res) => {
   // res.end();
 
   res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
   // send an html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
     } else {
